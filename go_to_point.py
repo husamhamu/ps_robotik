b9d811ae6964db6fc61887a339729429c81ce47d
Husam <husssamhamo123@gmail.com>
2023-06-28 16:52:29 +0200

    update to follow point

--- go_to_point.py
+++ go_to_point.py
@@ -16,7 +16,7 @@ def follow_point(goal_point, motor):
 
     listener = tf.TransformListener()
 
-    rate = rospy.Rate(3)  # Rate of 1 Hz
+    rate = rospy.Rate(8)  # Rate of 1 Hz
     goal_x = goal_point[0] *100
     goal_y = goal_point[1] *100
     
@@ -44,11 +44,11 @@ def follow_point(goal_point, motor):
             #determine the left and right speed of wheels
             robot_x, robot_y, robot_orientation = x*100, y*100, math.radians(yaw_degrees)
             #rospy.loginfo("Frame: /your_frame_name, Position: [robot_x: %.2f, robot_y: %.2f, robot_orientation: %.2f]", robot_x, robot_y,yaw_degrees)
-            print("follow_point(): goal_point", goal_point)
-            print("follow_point(): robot_point", str((x, y)))
+            #print("follow_point(): goal_point", goal_point)
+            #print("follow_point(): robot_point", str((x, y)))
             left_speed, right_speed = calculate_pid_controller(robot_x, robot_y, robot_orientation, goal_x, goal_y, 1.0)
             #rospy.loginfo("posereader: , : [left_speed: %.2f, right_speed: %.2f]", left_speed, right_speed) 
-            print("follow_point(): left_speed, right_speed ", left_speed, right_speed)
+            #print("follow_point(): left_speed, right_speed ", left_speed, right_speed)
             #set speed of wheels
             motor.set_speed(left_speed, right_speed)
 
