1cca50e41cbe07c613bc41aaa5087c80b033e12f
Husam <husssamhamo123@gmail.com>
2023-07-05 20:08:36 +0200

    fix following the point

--- go_to_point.py
+++ go_to_point.py
@@ -14,55 +14,44 @@ from std_msgs.msg import String
 import os 
 from yolo_detection import run_detect
 
+def inverse_transformer(listener, new_yaw):
+    # Look up camera pose with respect to arena
+    (trans, rot) = listener.lookupTransform('/csi://0', '/arena', rospy.Time(0))           
+    translation_matrix = tf.transformations.translation_matrix(trans)
+    rotation_matrix = tf.transformations.quaternion_matrix(rot)
+
+    #build inverse
+    R_inv = rotation_matrix.transpose()
+    print('R_inv ',R_inv)
+    t_inv = -np.dot(R_inv[:3,:3], translation_matrix[:3,3])
+    print('translation_matrix ',translation_matrix)
+    print('t_inv ',t_inv)
+    x, y, z = t_inv
+    T_inv = np.eye(4)
+    T_inv[:3, :3] = R_inv[:3,:3]
+    T_inv[:3, 3] = t_inv
+    #build 
+    # Create a quaternion from the rotation matrix
+    quaternion = tf.transformations.quaternion_from_matrix(R_inv)
+    roll, pitch, yaw = tf.transformations.euler_from_quaternion(quaternion)
+
 def follow_point(goal_point, motor):
     pub = rospy.Publisher('string_message_topoic', String, queue_size=10)
     rospy.init_node('tf_listener_node', anonymous=True)
 
     listener = tf.TransformListener()
 
-    rate = rospy.Rate(10)  # Rate of 1 Hz
+    rate = rospy.Rate(6)  # Rate of 1 Hz
     goal_x = goal_point[0] *100
     goal_y = goal_point[1] *100
-    
-    # To control the motor wheels speed
-    # motor = MotorControllerWaveshare()
+
     # PID controller gains (adjust these based on your requirements)
-    Kp = 0.97
+    Kp = 1.0
     Ki = 0.0
     Kd = 0.0
     pid_controller = PIDController(Kp, Ki, Kd)
     while not rospy.is_shutdown():
         try:
-            # # Look up camera pose with respect to arena
-            # (trans, rot) = listener.lookupTransform('/csi://0', '/map', rospy.Time(0))           
-            # translation_matrix = tf.transformations.translation_matrix(trans)
-            # rotation_matrix = tf.transformations.quaternion_matrix(rot)
-            # #build inverse
-            # R_inv = rotation_matrix.transpose()
-            # t_inv = -np.dot(R_inv, translation_matrix)
-            # x, y, z = t_inv
-            # T_inv = np.eye(4)
-            # T_inv[:3, :3] = R_inv
-            # T_inv[:3, 3] = t_inv
-            # #build 
-            # # Create a quaternion from the rotation matrix
-            # quaternion = tf.transformations.quaternion_from_matrix(R_inv)
-            # roll, pitch, yaw = tf.transformations.euler_from_quaternion(quaternion)
-
-            # #determine the left and right speed of wheels
-            # robot_x, robot_y, robot_orientation = x*100, y*100, yaw
-
-            # left_speed, right_speed, robot_orientation = calculate_pid_controller(robot_x, robot_y, robot_orientation, goal_x, goal_y, 1.0)
-            
-            # #set speed of wheels
-            # motor.set_speed(left_speed, right_speed)
-
-            # # If robot is close enough to goal point
-            # if left_speed == 0.0 and right_speed ==0.0:
-            #     time.sleep(1)
-            #     return (robot_x/100, robot_y/100), T_inv, yaw
-
-
             # Look up camera pose with respect to arena
             (trans, rot) = listener.lookupTransform('/map', '/csi://0', rospy.Time(0))
             x, y, z = trans
@@ -70,15 +59,12 @@ def follow_point(goal_point, motor):
 
             # Read angle values
             roll, pitch, yaw = tfs.euler_from_quaternion(rot)
-            roll_degrees = math.degrees(roll)
-            pitch_degrees = math.degrees(pitch)
-            yaw_degrees = math.degrees(yaw)
-            # rospy.loginfo("Frame: /your_frame_name, Orientation: [roll: %.2f, pitch: %.2f, yaw: %.2f]", roll_degrees, pitch_degrees, yaw_degrees)
 
             #determine the left and right speed of wheels
             robot_x, robot_y, robot_orientation = x*100, y*100, yaw
 
-            left_speed, right_speed, robot_orientation = calculate_pid_controller(robot_x, robot_y, robot_orientation, goal_x, goal_y, 1.0)
+            left_speed, right_speed, robot_orientation = calculate_pid_controller(robot_x, robot_y, robot_orientation, goal_x, goal_y, pid_controller, 1.0)
+            #print("follow_point(): left_speed, right_speed ", left_speed, right_speed)
 
             #set speed of wheels
             motor.set_speed(left_speed, right_speed)
@@ -92,7 +78,7 @@ def follow_point(goal_point, motor):
                 translation_matrix = tf.transformations.translation_matrix(trans)
                 rotation_matrix = tf.transformations.quaternion_matrix(rot)
                 transformation_matrix = np.dot(translation_matrix, rotation_matrix)
-                print("transformation_matrix ", transformation_matrix)
+                #print("transformation_matrix ", transformation_matrix)
                 robot_x, robot_y = x, y
                 return (robot_x, robot_y), transformation_matrix, robot_orientation
             
@@ -100,4 +86,3 @@ def follow_point(goal_point, motor):
             rospy.logwarn("Failed to lookup transform for frame: /your_frame_name")
         
         rate.sleep()
-
